857401e8cc1a6b4a6e0827936bb85a9d340fa6e8
peb7544 <peb7544@naver.com>
2024-04-26 14:29:41 +0900

    first pet

---
+++ petnolza/src/test/java/edu/kh/pet/PetnolzaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kh.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetnolzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
