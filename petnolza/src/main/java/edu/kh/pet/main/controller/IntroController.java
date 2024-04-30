e8d44ba7ac585e4c77a6c074663b4f4908419720
psw0712 <156268089+psw0712@users.noreply.github.com>
2024-04-30 16:36:35 +0900

    Revert "240430 peb"

--- petnolza/src/main/java/edu/kh/pet/main/controller/IntroController.java
+++ petnolza/src/main/java/edu/kh/pet/main/controller/IntroController.java
@@ -8,30 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("main")
 public class IntroController {
 	
-	/** 자주묻는질문
-	 * @return
-	 */
 	@GetMapping("qna")
 	public String qna() {
 		
 		return "main/qna";
-	}
-	
-	/** 서비스
-	 * @return
-	 */
-	@GetMapping("service")
-	public String service() {
-		
-		return "main/service";
-	}
-	
-	/** 오시는길
-	 * @return
-	 */
-	@GetMapping("route")
-	public String route() {
-		
-		return "main/route";
 	}
 }
