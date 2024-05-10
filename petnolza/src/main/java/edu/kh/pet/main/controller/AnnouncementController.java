71a7f24fa702b164d19f75e3986987f5ad3baee0
NIMGNUOY <ssong9214@gmail.com>
2024-05-10 16:02:58 +0900

    240510 sym

--- petnolza/src/main/java/edu/kh/pet/main/controller/AnnouncementController.java
+++ petnolza/src/main/java/edu/kh/pet/main/controller/AnnouncementController.java
@@ -1,18 +1,40 @@
 package edu.kh.pet.main.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import edu.kh.pet.community.model.service.CommunityService;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("announcement")
+@RequiredArgsConstructor
 public class AnnouncementController {
+	
+	private final CommunityService service;
+	public static String NoticeBoardCodeNo = "NOTICE";
+
 
 	/** 공지사항 목록
 	 * @return
 	 */
 	@GetMapping("announcementList")
-	public String announcementList() {
+	public String announcementList(@RequestParam(value="cp", required=false, defaultValue="1") int cp,
+								   Model model,
+								   @RequestParam Map<String, Object> paramMap) {
+		
+		Map<String, Object> map = null;
+		
+		if(paramMap.get("key") == null || paramMap.get("query") == "") {
+			
+			map = service.selectBoardList(NoticeBoardCodeNo, cp);
+			
+		}
 		
 		return "announcement/announcementList";
 	}
@@ -26,3 +48,13 @@ public class AnnouncementController {
 		return "announcement/announcementDetail";
 	}
 }
+
+
+
+
+
+
+
+
+
+
