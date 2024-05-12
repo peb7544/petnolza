763f61055030d73c5435b6e9035b5fcc9680f72e
peb7544 <peb7544@naver.com>
2024-05-13 07:03:20 +0900

    240513 peb

---
+++ petnolza/src/main/java/edu/kh/pet/room/model/dto/Room.java
@@ -0,0 +1,29 @@
+package edu.kh.pet.room.model.dto;
+import java.util.List;
+
+import edu.kh.pet.common.model.dto.UploadFile;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/* 객실 */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class Room {
+	private int roomId; // 객실 번호
+	private String roomName; // 객실명
+	private int roomPrice; // 객실 가격
+	private String roomInfo; // 객실 상세정보
+	
+	private List<String> roomInfoList; // 편의시설 리스트
+	private List<UploadFile> imageList; // 객실 이미지
+	private int thumnailYn; // 썸네일여부
+}
--- petnolza/src/main/java/edu/kh/pet/room/model/service/RoomServiceImpl.java
+++ petnolza/src/main/java/edu/kh/pet/room/model/service/RoomServiceImpl.java
@@ -3,8 +3,10 @@ package edu.kh.pet.room.model.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.kh.pet.common.model.dto.CodeMt;
+import edu.kh.pet.room.model.dto.Room;
 import edu.kh.pet.room.model.mapper.RoomMapper;
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +21,16 @@ public class RoomServiceImpl implements RoomService  {
 	public List<CodeMt> selectCodeList(String groupCode) {
 		
 		return mapper.selectCodeList(groupCode);
+	}
+
+	// 객실 등록
+	@Override
+	public int insertRoom(Room inputRoom, List<MultipartFile> images) {
+		
+		// 객실 등록
+		int roomId = mapper.insertRoom(inputRoom);
+		
+		return 0;
 	}
 
 }
